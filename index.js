47fc825ce867d6829c368a7770b2b90f83e4e3ed
HSM-Akiramenai <115085023+HSM-Akiramenai@users.noreply.github.com>
2024-07-15 18:39:43 +0500

    Update guest header

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-let onePointHomeEl = document.getElementById("1p-btn-home");
-let twoPointHomeEl = document.getElementById("2p-btn-home");
-let threePointHomeEl = document.getElementById("3p-btn-home");
-let onePointGuestEl = document.getElementById("1p-btn-guest");
-let twoPointGuestEl = document.getElementById("2p-btn-guest");
-let threePointGuestEl = document.getElementById("3p-btn-guest");
 let homeEl = document.getElementById("home");
 let guestEl = document.getElementById("guest");
 
@@ -13,62 +7,58 @@ let guestScoreEl = document.getElementById("guest-score-el");
 let homeTitleEl = document.getElementById("home-title-el");
 let guestTitleEl = document.getElementById("guest-title-el");
 
-homeTitleEl.textContent = "HOME"
-guestTitleEl.textContent = "GUEST"
+homeTitleEl.textContent = "HOME";
+guestTitleEl.textContent = "AWAY";
 
-homeScoreEl.textContent = "0"
-guestScoreEl.textContent = "0"
+homeScoreEl.textContent = "0";
+guestScoreEl.textContent = "0";
 
-let homeScore = 0
-let guestScore = 0
+let homeScore = 0;
+let guestScore = 0;
 
 function giveLead() {
-    if (homeScore > guestScore) {
-        // Home team is leading
-        homeEl.style.backgroundImage = 'url("crown.png")';
-    } else if (guestScore > homeScore) {
-        // Guest team is leading
-        guestEl.style.backgroundImage = 'url("crown.png")';
-    } else {
-        // Scores are tied
-        homeEl.style.backgroundImage = 'none'; // or set a default background image
-        guestEl.style.backgroundImage = 'none';
-    }
+  if (homeScore > guestScore) {
+    homeEl.style.backgroundImage = 'url("crown.png")';
+  } else if (guestScore > homeScore) {
+    guestEl.style.backgroundImage = 'url("crown.png")';
+  } else {
+    homeEl.style.backgroundImage = "none";
+    guestEl.style.backgroundImage = "none";
+  }
 }
 
-
 function addOnePointHome() {
-    homeScore += 1;
-    homeScoreEl.textContent = homeScore;
-    giveLead();
+  homeScore += 1;
+  homeScoreEl.textContent = homeScore;
+  giveLead();
 }
 
 function addTwoPointsHome() {
-    homeScore += 2;
-    homeScoreEl.textContent = homeScore;
-    giveLead();
+  homeScore += 2;
+  homeScoreEl.textContent = homeScore;
+  giveLead();
 }
 
 function addThreePointsHome() {
-    homeScore += 3;
-    homeScoreEl.textContent = homeScore;
-    giveLead();
+  homeScore += 3;
+  homeScoreEl.textContent = homeScore;
+  giveLead();
 }
 
 function addOnePointGuest() {
-    guestScore += 1;
-    guestScoreEl.textContent = guestScore;
-    giveLead();
+  guestScore += 1;
+  guestScoreEl.textContent = guestScore;
+  giveLead();
 }
 
 function addTwoPointsGuest() {
-    guestScore += 2;
-    guestScoreEl.textContent = guestScore;
-    giveLead();
+  guestScore += 2;
+  guestScoreEl.textContent = guestScore;
+  giveLead();
 }
 
 function addThreePointsGuest() {
-    guestScore += 3;
-    guestScoreEl.textContent = guestScore;
-    giveLead();
+  guestScore += 3;
+  guestScoreEl.textContent = guestScore;
+  giveLead();
 }
